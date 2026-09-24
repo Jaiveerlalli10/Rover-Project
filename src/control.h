a531e77215c018f356f83b396239297231a3a585
Jaiveer Lalli <jaiveerlalli@yahoo.com>
2026-09-24 16:04:45 -0700

    Add RoverMode enum and related functions

--- src/control.h
+++ src/control.h
@@ -1,6 +1,16 @@
 #ifndef CONTROL_H
 #define CONTROL_H
 
+enum RoverMode
+{
+    MANUAL,
+    AUTONOMOUS
+};
+
+RoverMode getRoverMode();
+
+void setRoverMode(RoverMode mode);
+
 bool isEmergencyStopped();
 
 void setEmergencyStop(bool stopped);
