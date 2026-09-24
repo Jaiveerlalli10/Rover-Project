193bdada57da924a0c19527fa470960f4c0b4242
Jaiveer Lalli <jaiveerlalli@yahoo.com>
2026-09-24 16:04:25 -0700

    Update control.cpp

--- src/control.cpp
+++ src/control.cpp
@@ -1,7 +1,34 @@
 #include "control.h"
 #include "motors.h"
+#include <Arduino.h>
+
 
 bool emergencyStop = false;
+
+RoverMode currentMode = MANUAL;
+
+
+
+RoverMode getRoverMode()
+{
+    return currentMode;
+}
+
+void setRoverMode(RoverMode mode)
+{
+    currentMode = mode;
+
+    if (mode == MANUAL)
+    {
+        Serial.println("MODE: MANUAL");
+    }
+    else if (mode == AUTONOMOUS)
+    {
+        Serial.println("MODE: AUTONOMOUS");
+    }
+}
+
+
 
 bool isEmergencyStopped()
 {
@@ -17,3 +44,7 @@ void setEmergencyStop(bool stopped)
         stopRobot();
     }
 }
+
+
+
+
